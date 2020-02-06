e66aac55453da8c0d928a48ed542090a7b2af36c
ggb2312 <gjgb2312@gmail.com>
2020-02-06 21:13:14 +0800

    提交无版本文件

---
+++ java-basic/interview/src/main/java/cn/lastwhisper/interview/string/StringTest.java
@@ -0,0 +1,15 @@
+package time;
+
+/**
+ * @author lastwhisper
+ */
+public class StringTest {
+
+    public void test() {
+        String s = "a";
+        s = s + "b" + "c";
+        s = s + "b" + "c" + "d";
+        System.out.println(s);
+    }
+
+}
