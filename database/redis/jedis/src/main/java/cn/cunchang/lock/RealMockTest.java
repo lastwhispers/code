ba37b3b17b8e85df3e790bec342ea63327e37e3a
寸长 <cunchang@dian.so>
2020-11-19 10:28:09 +0800

    feat(jedis): lock

---
+++ database/redis/jedis/src/main/java/cn/cunchang/lock/RealMockTest.java
@@ -0,0 +1,17 @@
+package cn.cunchang.lock;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 分布式锁，并发环境模拟
+ * @author cunchang
+ */
+public class RealMockTest {
+
+    ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
+
+
+}
