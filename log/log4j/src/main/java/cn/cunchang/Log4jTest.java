6e3831d30db0f21a0e20a95e5457ddc222a015f6
cunchang <15037584397@163.com>
2020-11-30 16:32:50 +0800

    feat(log): java日志jcl、log4j

--- log/log4j/src/main/java/cn/cunchang/Log4jTest.java
+++ log/log4j/src/main/java/cn/cunchang/Log4jTest.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 
 
 /**
- * 官方网站: http://logging.apache.org/log4j/1.2/
+ *
+ *
+ * @author cunchang
  */
 public class Log4jTest {
 
@@ -31,25 +33,18 @@ public class Log4jTest {
         // 初始化系统配置，不需要配置文件
         BasicConfigurator.configure();
         // 创建日志记录器对象
-        Logger logger = Logger.getLogger(this.getClass().getName());
+        Logger logger = Logger.getLogger(this.getClass());
         // 日志记录输出
         logger.info("hello log4j");
 
         // 日志级别
-        // 严重错误，一般会造成系统崩溃和终止运行
-        logger.fatal("fatal");
-        // 错误信息，但不会影响系统运行
-        logger.error("error");
-        // 警告信息，可能会发生问题
-        logger.warn("warn");
-        // 程序运行信息，数据库的连接、网络、IO操作等
-        logger.info("info");
-        // 调试信息，一般在开发阶段使用，记录程序的变量、参数
-        logger.debug("debug");
-        // 追踪信息，记录程序的所有流程信
-        logger.trace("trace");
+        logger.fatal("fatal");// 严重错误，一般会造成系统崩溃和终止运行
+        logger.error("error");// 错误信息，但不会影响系统运行
+        logger.warn("warn");// 警告信息，可能会发生问题
+        logger.info("info");// 程序运行信息，数据库的连接、网络、IO操作等
+        logger.debug("debug");// 调试信息，一般在开发阶段使用，记录程序的变量、参数
+        logger.trace("trace");// 追踪信息，记录程序的所有流程信
     }
-
 
     @Test
     public void testCustomLogger() {
