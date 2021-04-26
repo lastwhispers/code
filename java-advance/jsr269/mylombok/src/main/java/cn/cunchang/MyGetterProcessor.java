f407fb2fa5a9f8fa5f2eeeb56c2fc8f118d2c682
cunchang <lastwhisper@yeah.net>
2021-04-27 00:02:31 +0800

    注解处理

--- java-advance/jsr269/mylombok/src/main/java/cn/cunchang/MyGetterProcessor.java
+++ java-advance/jsr269/mylombok/src/main/java/cn/cunchang/MyGetterProcessor.java
@@ -1,6 +1,5 @@
 package cn.cunchang;
 
-import com.google.auto.service.AutoService;
 import com.sun.source.tree.Tree;
 import com.sun.tools.javac.api.JavacTrees;
 import com.sun.tools.javac.code.Flags;
