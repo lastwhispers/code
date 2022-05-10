1ad587223c18b34e220f6507b64ca9b5788e512e
lastwhispers <lastwhisper@yeah.net>
2022-05-10 10:55:26 +0800

    future

--- java-basic/concurrent/src/main/java/cn/lastwhisper/concurrent/example/future/FutureData.java
+++ java-basic/concurrent/src/main/java/cn/lastwhisper/concurrent/example/future/FutureData.java
@@ -3,25 +3,25 @@ package cn.lastwhisper.concurrent.example.future;
 //当有线程想要获取RealData时候，程序会被阻塞。等到RealData被注入才会使用getReal()方法
 public class FutureData extends Data {
 
-    private boolean FLAG = false;
+    private boolean isReady = false;
     private RealData realData;
 
     //读取data数据
     public synchronized void setRealData(RealData realData) {
         //读取结果
-        if (FLAG) {  //true 说明已经获取到结果了 如果获取到则直接返回结果
-            System.out.println("FLAG：" + FLAG);
+        if (isReady) {  //true 说明已经获取到结果了 如果获取到则直接返回结果
+            System.out.println("FLAG：" + isReady);
         }
         //如果flag是false，没有获取到数据，传递realData对象
         this.realData = realData;
-        FLAG = true;  //获取到执行结果 改为true
-        notify();//唤醒
+        isReady = true;  //获取到执行结果 改为true
+        notifyAll();//唤醒
     }
 
 
     @Override
     public synchronized String getRequest() {
-        while (!FLAG) {  //如果false 一直等待
+        while (!isReady) {  //如果false 一直等待
             try {
                 wait();
             } catch (InterruptedException e) {
--- java-basic/concurrent/src/main/java/cn/lastwhisper/concurrent/example/future/RealData.java
+++ java-basic/concurrent/src/main/java/cn/lastwhisper/concurrent/example/future/RealData.java
@@ -6,14 +6,14 @@ public class RealData extends Data {
     private String requestData;
 
     public RealData(String requestData) {
-        System.out.println("正在使用data进行网络请求，data" + requestData + "开始");
+        System.out.println("RealData——————正在使用requestData进行网络请求,requestData:[" + requestData + "],开始");
         try {
             //模拟执行业务逻辑耗时时间
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("操作执行完毕...获取结果");
+        System.out.println("RealData——————操作执行完毕...获取结果");
         //获取返回结果
         this.requestData = "结果";
     }
